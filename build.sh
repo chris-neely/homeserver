dadff1fd864b1dd432966e8e31277383c9c46fb6
Chris Neely <chris-neely@users.noreply.github.com>
2025-03-15 21:12:48 -0500

    initial test of incus and inventory

--- build.sh
+++ build.sh
@@ -10,9 +10,60 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux 
 
-# Use a COPR Example:
+# List of Packages to Install
+PACKAGES=(
+    incus
+    cronie
+)
+
+# Install Packages
+dnf5 -y install "${PACKAGES[@]}"
+
+# Install Inventory
+# https://bketelsen.github.io/inventory/docs/installation/#installer-script 
+sh -c "$(curl --location https://bketelsen.github.io/inventory/install.sh)" -- -d -b /usr/local/bin
+
+# Inventory Config File
+# https://github.com/bketelsen/inventory/blob/main/README.md
+mkdir -p ~/.inventory
+tee ~/.inventory/inventory <<'EOF'
+server:
+  address: "127.0.0.1:9999"
+verbose: false
+EOF
+
+# Inventory Service
+tee /etc/systemd/system/inventory-server.service <<'EOF'
+[Unit]
+Description=Inventory Server
+After=network.target
+Requires=network.target
+
+[Service]
+Type=notify
+
+Restart=always
+RestartSec=30
+TimeoutStartSec=0
+
+WorkingDirectory=/var/home/core
+ExecStart=/usr/local/bin/inventory serve
+User=core
+Group=core
+NoNewPrivileges=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+#Inventory CRON Job
+# https://bketelsen.github.io/inventory/docs/cli/inventory_send/
+echo "*/2 * * * * /usr/local/bin/inventory send  >> /var/log/inventory.log 2>&1" >> /etc/crontab
+
+
+## Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
@@ -21,4 +72,6 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
+systemctl enable inventory-server.service
+systemctl enable crond.service
